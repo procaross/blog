7b79d46ff24b316f320cba9d03fbe6a0386dc143
proca <m2101250877@gmail.com>
2022-09-02 22:01:59 +0800

    fix: fix the website name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Proca的个人博客',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
