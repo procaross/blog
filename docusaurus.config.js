54446604222d3baa6815272a600e58f0c026fecd
Proca <m2101250877@gmail.com>
2023-02-28 13:42:36 +0800

    fix: 删除博客底栏“编辑此页”

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,10 +62,6 @@ const config = {
           path: "./blog",
           routeBasePath: "/blog",
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
